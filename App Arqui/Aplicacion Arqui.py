9b1fe7e58263746e6226626596beaad6ea419504
04esteban04 <estebanmadrigal7@gmail.com>
2023-11-18 11:54:25 -0600

    Cambios en interfaz

--- App Arqui/Aplicacion Arqui.py
+++ App Arqui/Aplicacion Arqui.py
@@ -13,28 +13,29 @@ class App:
     def __init__(self, window):
         self.window = window
         self.window.title('Robot de Humedad')
-        self.window.geometry("750x450")
-        self.window.resizable(False,False)
-        
+        self.window.geometry("1050x450")
+        self.window.resizable(False,False)        
 
-        #Button
-        self.devices_button = Button(window, text="Buscar", font=('Calibri',12), command=self.get_devices).place(x=305,y=150)
-        self.connect_button = Button(window, text="Conectar", font=('Calibri',12),command=self.connect).place(x=300,y=200)
-        self.average_button = Button(window, text="Obtener Promedio", font=('Calibri',12)).place(x=450,y=380)
+        #Button (Conexion)
+        self.devices_button = Button(window, text="Buscar", font=('Calibri',12), command=self.get_devices).place(x=770,y=50)
+        self.connect_button = Button(window, text="Conectar", font=('Calibri',12),command=self.connect).place(x=900,y=50)
+        self.average_button = Button(window, text="Obtener Promedio", font=('Calibri',12)).place(x=790,y=320)
 
-         # Listbox
-        self.devices_listbox = Listbox(window, width=33, height=12)
-        self.devices_listbox.place(x=40, y=80)
+        # Listbox (Dispositivos disponibles)
+        self.devices_listbox = Listbox(window, width=28, height=9)
+        self.devices_listbox.place(x=35, y=75)
 
-        # Text Space
-        self.data_text = Text(window, width=25, height=12, wrap=WORD)
-        self.data_text.place(x=415, y=80)
-        self.noti_text = Text(window, width=18, height=6, wrap=WORD)
-        self.noti_text.place(x=260, y=320)
-        self.promedio = Label(window,width=20, height=2).place(x=443,y=322)
+        # Text Space (Datos sensor humedad)
+        self.data_text = Text(window, width=31, height=9, wrap=WORD)
+        self.data_text.place(x=410, y=75)
+
+        # Text Space (Alerta conexion)
+        self.noti_text = Text(window, width=25, height=3, wrap=WORD)
+        self.noti_text.place(x=770, y=130)
+
+        # Promedio de datos
+        self.promedio = Label(window,width=20, height=2).place(x=435,y=320)
     
-    
-
     def get_devices(self):
         nearby_devices = bluetooth.discover_devices()
         self.devices_listbox.delete(0, END)  # Limpiar la lista antes de agregar nuevos dispositivos
@@ -81,16 +82,18 @@ class App:
             print("Error al recibir datos:", err)
         finally:
             self.device.close()
+
 #Function to load images
 def load_image(name):
-    rute = os.path.join ("Imagenes", name)
+    #rute = os.path.join ("Imagenes", name)
+    rute = os.path.join ("/home/esteban/Desktop/P3-Arqui/computer_architecture_p3_IIS_2023/App Arqui/Imagenes/", name)
     image = PhotoImage (file = rute)
     return image
 
 if __name__ == "__main__":
     window = Tk()
     bgMain = load_image("Principal.png")
-    P_canvas= Canvas (window, width = 750, height = 450, bg = "black")
+    P_canvas= Canvas (window, width = 1050, height = 450, bg = "black")
     P_canvas.place (x = 0, y = 0)
     ImgCanvas = P_canvas.create_image (0, 0, anchor = NW, image = bgMain)
     app = App(window)
